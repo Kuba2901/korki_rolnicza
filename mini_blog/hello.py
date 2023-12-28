f21b8af8567f56d5cdd7feb57d52f6293e8fd15f
Kuba Nenczak <kubanenczak@gmail.com>
2023-12-28 14:09:54 +0100

    links

--- mini_blog/hello.py
+++ mini_blog/hello.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+# https://www.freecodecamp.org/news/how-to-dockerize-a-flask-app/
